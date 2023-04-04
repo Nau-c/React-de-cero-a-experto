a8f79edd5f6333dd1dd0c6d8bd0b34063d198861
Nau-c <nlopez@desic-sl.com>
2023-04-04 11:52:27 +0100

    01-const-let

---
+++ 02-INTRO-JAVASCRIPT/src/bases/01-const-let.js
@@ -0,0 +1,19 @@
+
+// console.log('Hola nau');
+
+const nombre = 'Nauzet';
+const apellido = 'López Mendoza';
+
+let valorDado = 5;
+valorDado = 4;
+
+
+console.log(nombre, apellido, valorDado);
+
+//var no se debe de usar
+if ( true ) {
+    const nombre = 'Nauzet';
+    console.log( valorDado );
+}
+
+console.log( valorDado );
