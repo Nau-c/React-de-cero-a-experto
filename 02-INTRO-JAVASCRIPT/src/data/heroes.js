eee4df43f7feec8a8a1ce165db9983166b2ef391
Nau-c <nlopez@desic-sl.com>
2023-04-09 02:41:11 +0100

    08-imp-exp

---
+++ 02-INTRO-JAVASCRIPT/src/data/heroes.js
@@ -0,0 +1,64 @@
+//Otra forma de hacerlo con default
+// export default  [
+//     {
+//         id: 1,
+//         name: 'Batman',
+//         owner: 'DC'
+//     },
+//     {
+//         id: 2,
+//         name: 'Spiderman',
+//         owner: 'Marvel'
+//     },
+//     {
+//         id: 3,
+//         name: 'Superman',
+//         owner: 'DC'
+//     },
+//     {
+//         id: 4,
+//         name: 'Flash',
+//         owner: 'DC'
+//     },
+//     {
+//         id: 5,
+//         name: 'Wolverine',
+//         owner: 'Marvel'
+//     },
+// ];
+
+const heroes = [
+    {
+        id: 1,
+        name: 'Batman',
+        owner: 'DC'
+    },
+    {
+        id: 2,
+        name: 'Spiderman',
+        owner: 'Marvel'
+    },
+    {
+        id: 3,
+        name: 'Superman',
+        owner: 'DC'
+    },
+    {
+        id: 4,
+        name: 'Flash',
+        owner: 'DC'
+    },
+    {
+        id: 5,
+        name: 'Wolverine',
+        owner: 'Marvel'
+    },
+];
+
+export const owners = ['DC', 'Marvel']
+export default heroes;
+
+ // export {
+//     heroes as default, 
+//     owners
+// }
