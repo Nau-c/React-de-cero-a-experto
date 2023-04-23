cfd5550ed01c4ebfc7d079cdaa9f993eb4763d13
Nauzet Lopez Mendoza <nauzetlm90@gmail.com>
2023-04-23 21:21:28 +0100

    call functions in components

--- 03-counter-app-vite/src/FirtsApp.jsx
+++ 03-counter-app-vite/src/FirtsApp.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 
-export const FirtsApp = () => {
+const message = {
+    message: 'Probando el objeto',
+    title: 'Nauzet'
+
+}
+
+const getSaludo = () => {
+    if ( message ) {
+        return <h2>{ message.message }</h2>
+    } else {
+        return <h2>Saludo por defecto</h2>
+    }
+}
+
+    export const FirtsApp = () => {
 
     return (
         <>
-        <h2>FirtsApp</h2>
-        <p>Soy un subtitulo</p>
+            <h1> { getSaludo() } </h1>
+            <h2>FirtsApp</h2>
+            <p>Soy un subtitulo</p>
+            {/* <p>{ JSON.stringify( message ) }</p> */}
         </>
 
     )
