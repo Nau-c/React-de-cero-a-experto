dbdc8b3d7cf35737b29435d8bdf7a6d10639ef0d
Nauzet Lopez Mendoza <nauzetlm90@gmail.com>
2023-05-01 19:01:44 +0100

    fixed

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,26 +1,26 @@
+import { useState } from 'react'
 import PropTypes from 'prop-types';
+
 export const CounterApp = ({ value }) => {
+
+    const [ counter, setCounter ] = useState( value );
     
+    //hooks son constantes para no setear el valor
     const handleAdd = () => {
-        console.log('+3');
-        value = 100
-        console.log(value)
+        // console.log('+3');
+        // value = 100
+        // console.log(value)
+        setCounter( (count) => count + 1)
     }
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> { counter } </h2>
             <button onClick={ handleAdd }>
                 +1
             </button>
         </>
-    )
-
-}
-
-
-CounterApp.PropTypes = {
-    value: PropTypes.number.isRequired,
+    );
 
 }
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App, HelloWorldApp } from "./HelloWorldApp";
 import { CounterApp } from "./CounterApp";
+import { ComponenteG } from "./ComponenteG";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root") ).render(
@@ -15,8 +16,9 @@ ReactDOM.createRoot(document.getElementById("root") ).render(
     {/* <FirstApp title = "Espero un string" subTitle = {123}/> */}
     {/* >>>>>>> 2bc0dc4d44599596e292124730ab0b733f2419a4 */}
 
-    <CounterApp value = { 10 } />
-
+    <CounterApp value={20}/>
+    {/* <ComponenteG/> */}
+    
   </React.StrictMode>
 );
 
