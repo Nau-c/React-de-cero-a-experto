14e97754c3c143c6c4da14448f67dcab33146060
Nauzet Lopez Mendoza <nauzetlm90@gmail.com>
2023-05-01 18:20:43 +0100

    Event clik

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,10 +1,19 @@
 import PropTypes from 'prop-types';
-
 export const CounterApp = ({ value }) => {
+    
+    const handleAdd = () => {
+        console.log('+3');
+        value = 100
+        console.log(value)
+    }
+
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{ value }</h2>
+            <h2> { value } </h2>
+            <button onClick={ handleAdd }>
+                +1
+            </button>
         </>
     )
 
